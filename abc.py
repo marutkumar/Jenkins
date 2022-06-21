fafd7b7ef1566ad1c75673ce6408a7888998374b
marutkumar <82207991+marutkumar@users.noreply.github.com>
2022-06-21 23:23:04 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,13 +1,4 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-}
-
- #Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    name = "Default VPS"
-  }
-}
-
+a=6
+b=10
+c=a+b
+print(c)
